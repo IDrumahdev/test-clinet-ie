3f469770bb4fd1fa14ae723dff46577a924b5fcc
IDrumahdev <ibnudirsan@gmail.com>
2023-12-15 06:47:56 +0700

    Transaksi Masuk

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,4 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/master-barang','MasterBarangController::index');
+$routes->get('/transaksi-masuk','TransaksiMasukController::index');
--- app/Controllers/TransaksiMasukController.php
+++ app/Controllers/TransaksiMasukController.php
@@ -5,18 +5,18 @@ namespace App\Controllers;
 use Config\Services;
 use App\Controllers\BaseController;
 
-class MasterBarangController extends BaseController
+class TransaksiMasukController extends BaseController
 {
     public function index()
     {
         $client = Services::curlrequest();
-        $response = $client->request('GET', 'http://localhost:8080/api/v1/barang');
+        $response = $client->request('GET', 'http://localhost:8080/api/v1/transaksi-masuk');
         $result = json_decode($response->getBody());
-        // dd($result->result[0]->id_barang);
-        // exit;
+
+        // dd($result->results[0]->id_transaksi_masuk);
 
         echo view('layouts/header');
         echo view('layouts/navbar');
-        echo view('barang/index',['result' => $result]);
+        echo view('masuk/index',['result' => $result]);
     }
 }
